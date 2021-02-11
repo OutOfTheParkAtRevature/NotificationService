fd2271018a8871ab0c143ceea82573414453af77
joelbarnum <joelbarnum@yahoo.com>
2021-02-11 11:16:35 -0800

    documentation

--- Repo/Repo.cs
+++ Repo/Repo.cs
@@ -18,22 +18,37 @@ namespace Repository
             _logger = logger;
             this.notifications = _notificationContext.Notifications;
         }
-
+        /// <summary>
+        /// saves changes to the database
+        /// </summary>
+        /// <returns></returns>
         public async Task CommitSave()
         {
             await _notificationContext.SaveChangesAsync();
         }
+        /// <summary>
+        /// return a list of all notifications
+        /// </summary>
+        /// <returns></returns>
         public async Task<IEnumerable<Notification>> GetNotifications()
         {
             return await notifications.ToListAsync();
         }
-
+        /// <summary>
+        /// adds a notification to the notifications DbSet
+        /// </summary>
+        /// <param name="notification"></param>
+        /// <returns></returns>
         public async Task AddNotification(Notification notification)
         {
             await notifications.AddAsync(notification);
         }
 
         // no async method for remove
+        /// <summary>
+        /// deletes a notification from the notification dbset
+        /// </summary>
+        /// <param name="notification"></param>
         public void DeleteNotification(Notification notification)
         {
             notifications.Remove(notification);
